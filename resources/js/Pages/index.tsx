7cc04312c6b3f6745356f54d29fe87155e94e45e
maskadev <sidkiyouness@gmail.com>
2022-04-24 01:13:49 +0000

    init project

---
+++ resources/js/Pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div className=" text-4xl">index</div>
+  )
+}
+
+export default index
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+      "./resources/**/*.blade.php",
+      "./resources/**/*.js",
+      "./resources/**/*.tsx",
+      "./resources/**/*.ts",
+      "./resources/**/*.vue",
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
